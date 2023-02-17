180af73cc908f0f779f474e18e1be1560083fdcf
donpedro45 <adeyinkaprince@gmail.com>
2023-02-17 11:05:21 -0800

    touch 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,21 @@
+int main() {
+    int a, b;
+    for (a = 0; a <= 99; a++) {
+        for (b = i; b <= 99; b++) {
+            int d1 = a / 10;
+            int d2 = a % 10;
+            int d3 = b / 10;
+            int d4 = b % 10;
+            putchar(d1 + '0');
+            putchar(d2 + '0');
+            putchar(' ');
+            putchar(d3 + '0');
+            putchar(d4 + '0');
+            if (!(a == 99 && b == 99)) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    return 0;
+}
