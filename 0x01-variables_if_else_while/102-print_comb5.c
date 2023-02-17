9840098367416dad49e3c9ac68ddc6c5c00832e5
donpedro45 <adeyinkaprince@gmail.com>
2023-02-17 11:15:08 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,21 +1,33 @@
-int main() {
-    int a, b;
-    for (a = 0; a <= 99; a++) {
-        for (b = a; b <= 99; b++) {
-            int d1 = a / 10;
-            int d2 = a % 10;
-            int d3 = b / 10;
-            int d4 = b % 10;
-            putchar(d1 + '0');
-            putchar(d2 + '0');
-            putchar(' ');
-            putchar(d3 + '0');
-            putchar(d4 + '0');
-            if (!(a == 99 && b == 99)) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    return 0;
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Program that prints the combination of two-digit numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{int first, second;
+for (first = 0; first < 100; first++)
+{
+for (second = 0; second < 100; second++)
+{
+if (first < second)
+{putchar((first / 10) + 48);
+putchar((first % 10) + 48);
+putchar(' ');
+putchar((second / 10) + 48);
+putchar((second % 10) + 48);
+if (first != 98 || second != 99)
+{putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
